2cacf40ab0a7db1bb567f3cff35af76e6252c3f8
art4829 <art4829@g.rit.edu>
2018-07-18 19:44:45 +0545

    ...

--- nba.py
+++ nba.py
@@ -10,32 +10,44 @@ lineUpList=[]
 # reads files and stores to according lists
 def read(name,lst):
     file = open(name)
-    line = file.readline()
+    line = file.readline().strip()
     line = file.readline().strip()
     while line != "":
         splitLine = line.split("\t")
+        # print(splitLine)
+        # print("")
         lst.append(splitLine)
         line = file.readline().strip()
     file.close()
 
+# def readplay():
+#     with open(playByPlay) as f:
+#         for line in f:
+#             playByPlayList.append(line.split())
+
+
+
+
+
 
 def main():
     read(playByPlay,playByPlayList)
-    read(eventCodes,eventList)
-    read(lineUp,lineUpList)
-    print(eventList)
-    # intializing the dict of players with their team id and plus/minus
-    players=dict()
-    for player in lineUpList[2]:
-        players[player]=[lineUpList[3],0]
-    # player Plus/Minus
-    scoringTeam = playByPlayList[10]  # scoring team id
-    for event in playByPlayList:
-        print(event)
-        print("here")
-        if event[2] == '1':
+    print(playByPlayList)
+    # read(eventCodes,eventList)
+    # read(lineUp,lineUpList)
+    # print(eventList)
+    # # intializing the dict of players with their team id and plus/minus
+    # players=dict()
+    # for player in lineUpList[2]:
+    #     players[player]=[lineUpList[3],0]
+    # # player Plus/Minus
+    # scoringTeam = playByPlayList[10]  # scoring team id
+    # for event in playByPlayList:
+    #     print(event)
+    #     print("here")
+    #     if event[2] == '1':
             # if there is a goal
-            print("true")
+            # print("true")
             # find team id
 
 #       if(event==1):
